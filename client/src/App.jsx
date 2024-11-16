f034b63b53c0d4b8d5c1b770fc49123979563464
Vithubhavani <146507010+Vithubhavani@users.noreply.github.com>
2024-11-16 14:14:23 +0530

    finished carousal page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage'
 
 function App() {
 
 
   return (
     <>
-  
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<RegisterPage/>} />
+      <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/home" element={<RegisterPage/>} />
+      <Route path="/carousal" element={<RegisterPage/>} />
+      <Route path="/dashboard" element={<RegisterPage/>} />
+      <Route path="/movies" element={<RegisterPage/>} />
+      <Route path="*" element={<RegisterPage/>} />
+    </Routes>
+  </BrowserRouter>
     </>
   )
 }
---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,9 @@
+import styles from './RegisterPage.module.css'
+
+export default function RegisterPage() {
+  return (
+    <div className={styles.container}>
+     Register Page 
+    </div>
+  )
+}
