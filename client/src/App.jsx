43ad620e4dc265441d8c6cedd6abb4117b720fec
Vithubhavani <146507010+Vithubhavani@users.noreply.github.com>
2024-11-18 23:05:56 +0530

    corrected and added vercel and protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,6 @@
+import { useContext } from 'react'
+import { AppContext } from './context/AppContext'
+import { Navigate } from 'react-router-dom'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import RegisterPage from './pages/RegisterPage'
 import NotFoundPage from './pages/NotFoundPage'
@@ -6,9 +9,12 @@ import CarousalPage from './pages/CarousalPage'
 import DashboardPage from './pages/DashboardPage'
 import MoviePage from './pages/MoviePage'
 
+
+const protectedRoute=({children})=>{
+  const{user}=useContext(AppContext)
+  return user?children:<Navigate to="/register"/>
+}
 function App() {
-
-
   return (
     <>
   <BrowserRouter>
@@ -16,10 +22,26 @@ function App() {
       <Route path="/" element={<RegisterPage/>} />
       <Route path="/register" element={<RegisterPage/>} />
       <Route path="/home" element={<RegisterPage/>} />
-      <Route path='/genre' element={<GenrePage/>}/>
-      <Route path="/carousel" element={<CarousalPage/>} />
-      <Route path="/dashboard" element={<DashboardPage/>} />
-      <Route path="/movies" element={<MoviePage/>} />
+      <Route path='/genre' element={
+        <protectedRoute>
+          <GenrePage/>
+        </protectedRoute>
+      }/>
+      <Route path="/carousel" element={
+        <protectedRoute>
+          <CarousalPage/>
+        </protectedRoute>
+      } />
+      <Route path="/dashboard" element={
+        <protectedRoute>
+          <DashboardPage/>
+        </protectedRoute>
+      } />
+      <Route path="/movies" element={
+        <protectedRoute>
+          <MoviePage/>
+        </protectedRoute>
+      } />
       <Route path="*" element={<NotFoundPage/>} />
       
     </Routes>
--- client/src/pages/DashboardPage.jsx
+++ client/src/pages/DashboardPage.jsx
@@ -1,18 +1,21 @@
 import styles from './DashboardPage.module.css'
 import  Timerwidget  from '../component/Timerwidget'
 import Notes from '../component/Notes'
+import Newswidget from '../component/Newswidget'
+import Profilewidget from '../component/Profilewidget'
+import Weatherwidget from '../component/Weatherwidget'
 
 export default function DashboardPage() {
   return (
     <div className={styles.container}>
  
- <div className={styles.profile}>div1</div>
-	<div className={styles.weather}>div2</div>
+ <div className={styles.profile}><Profilewidget/></div>
+	<div className={styles.weather}><Weatherwidget/></div>
 	<div className={styles.timer}>
     <Timerwidget/>
     </div>
 	<div className={styles.notes}><Notes/></div>
-	<div className={styles.news}>div5</div>
+	<div className={styles.news}><Newswidget/></div>
     </div>
   )
 }
